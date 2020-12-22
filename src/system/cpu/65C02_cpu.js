a39bfec39b7c32188b9660511b5c1acd3e77cb6d
Éric Bissonnette <erbisson@silabs.com>
2020-12-21 21:16:41 -0500

    feat: add 65C02 architecture

--- src/system/cpu/65C02_cpu.js
+++ src/system/cpu/65C02_cpu.js
@@ -17,11 +17,76 @@ class CPU65C02 extends CPU6502{
     // Subroutines - addressing modes & flags
     ////////////////////////////////////////////////////////////////////////////////
 
+	indx() {
+		var a = this.read(this.PC++);
+        a |= (this.read(this.PC++) << 8);
+        this.addr += this.X;
+		this.addr = this.read(a);
+		this.addr |= (this.read((a & 0xFF00) | ((a + 1) & 0xFF)) << 8);
+		this.cycles += 6;
+	}
+
+    iz() {
+		var a = this.read(this.PC++) & 0xFF;
+        this.addr |= (this.read((a & 0xFF00) | ((a + 1) & 0xFF)) << 8);
+        if (this.D) {
+            this.cycles += 5;
+        } else {
+
+        }
+    }
+
+    bra() { this.branch(true); }
+
+    dea() {
+		this.A = (this.A - 1) & 0xFF;
+		this.fnz(this.A);
+    }
+
     ina() {
 	    this.A = (this.A + 1) & 0xFF;
 	    this.fnz(this.A);
     }
 
+	phx() {
+		this.write(this.S + 0x100, this.X);
+		this.S = (this.S - 1) & 0xFF;
+		this.cycles++;
+    }
+
+	phy() {
+		this.write(this.S + 0x100, this.Y);
+		this.S = (this.S - 1) & 0xFF;
+		this.cycles++;
+    }
+
+	plx() {
+		this.S = (this.S + 1) & 0xFF;
+		this.X = this.read(this.S + 0x100);
+		this.fnz(this.X);
+		this.cycles += 2;
+    }
+
+	ply() {
+		this.S = (this.S + 1) & 0xFF;
+		this.Y = this.read(this.S + 0x100);
+		this.fnz(this.Y);
+		this.cycles += 2;
+    }
+
+	trb() {
+        this.tmp = this.read(this.addr);
+        this.Z = ((this.tmp & this.A) == 0) ? 1 : 0;
+        this.tmp = this.tmp & ~this.A;
+        this.write(this.addr, this.tmp);
+	}
+
+	tsb() {
+        this.tmp = this.read(this.addr);
+        this.Z = ((this.tmp & this.A) == 0) ? 1 : 0;
+        this.tmp = this.tmp | this.A;
+        this.write(this.addr, this.tmp);
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/system/cpu/65C02_opcodes.js
+++ src/system/cpu/65C02_opcodes.js
@@ -4,6 +4,42 @@
 
 var CPU65C02op = new Array();
 
+/*  AND iz  */ CPU65C02op[0x32] = function(m) { m.iz(); m.and(); };
+
+/*  ADC iz  */ CPU65C02op[0x72] = function(m) { m.iz(); m.adc(); };
+
+/*  BIT zp  */ CPU65C02op[0x34] = function(m) { m.zpx(); m.bit(); };
+/*  BIT abs */ CPU65C02op[0x3C] = function(m) { m.abx(); m.bit(); };
+/*  BIT zp  */ CPU65C02op[0x89] = function(m) { m.imm(); m.bit(); };
+
+/*  BRA rel */ CPU65C02op[0x80] = function(m) { m.rel(); m.bra(); };
+
+/*  CMP iz  */ CPU65C02op[0xC1] = function(m) { m.iz(); m.cmp(); };
+
+/*! DEC A   */ CPU65C02op[0x3A] = function(m) { m.imp(); m.dea(); };
+
+/*  EOR iz  */ CPU65C02op[0x41] = function(m) { m.iz(); m.eor(); };
+
 /*! INC A   */ CPU65C02op[0x1A] = function(m) { m.imp(); m.ina(); };
+
+/*  JMP indx */ CPU65C02op[0x7C] = function(m) { m.indx(); m.jmp(); };
+
+/*  LDA iz  */ CPU65C02op[0xB2] = function(m) { m.iz(); m.lda(); };
+
+/*  PHX     */ CPU65C02op[0xDA] = function(m) { m.imp(); m.phx(); };
+/*  PHY     */ CPU65C02op[0x5A] = function(m) { m.imp(); m.phy(); };
+
+/*  PLX     */ CPU65C02op[0xFA] = function(m) { m.imp(); m.plx(); };
+/*  PLY     */ CPU65C02op[0x7A] = function(m) { m.imp(); m.ply(); };
+
+/*  SBC iz  */ CPU65C02op[0xF2] = function(m) { m.iz(); m.sbc(); };
+
+/*  STA iz  */ CPU65C02op[0x81] = function(m) { m.iz(); m.sta(); };
+
+/*  TRB abs */ CPU65C02op[0x1C] = function(m) { m.abs(); m.trb(); };
+/*  TRB zp  */ CPU65C02op[0x14] = function(m) { m.zp(); m.trb(); };
+
+/*  TSB abs */ CPU65C02op[0x0C] = function(m) { m.abs(); m.tsb(); };
+/*  TSB zp  */ CPU65C02op[0x04] = function(m) { m.zp(); m.tsb(); };
 
 module.exports = CPU65C02op;
